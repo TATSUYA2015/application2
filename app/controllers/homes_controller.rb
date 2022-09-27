4c7e403f7708f69e48eca6835d4fd0bb69fc3ef8
EC2 Default User <112598954+TATSUYA2015@users.noreply.github.com>
2022-09-27 13:02:10 +0000

    [Finish] 4-22

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,23 @@
+class HomesController < ApplicationController
+  def top
+  end
+
+  def about
+
+  end
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
